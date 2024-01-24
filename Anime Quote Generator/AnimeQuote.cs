557e7bf5a46abf54ebf5469ca37e59b835026e6e
Zach H <huntzachary30@gmail.com>
2024-01-23 21:18:28 -0600

    Add project files.

---
+++ Anime Quote Generator/AnimeQuote.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace Anime_Quote_Generator
+{
+    internal class AnimeQuote
+    {
+        [JsonProperty("anime")]
+        public string? Anime { get; set; }
+
+        [JsonProperty("character")]
+        public string? Character { get; set; }
+
+        [JsonProperty("quote")]
+        public string? Quote { get; set; }
+
+        public override string ToString()
+        {
+            return $"{WrapText(Quote, 60)} - {Character ?? "Unknown Character"} ({Anime ?? "Unknown Anime"})";
+        }
+
+        private static string WrapText(string text, int maxLineLength)
+        {
+            if (text.Length <= maxLineLength)
+            {
+                return text;
+            }
+
+            var lines = new List<string>();
+
+            while (text.Length > maxLineLength)
+            {
+                int lastSpace = text.LastIndexOf(' ', maxLineLength);
+                if (lastSpace == -1)
+                {
+                    lastSpace = maxLineLength;
+                }
+
+                lines.Add(text.Substring(0, lastSpace));
+                text = text.Substring(lastSpace + 1);
+            }
+
+            lines.Add(text);
+
+            return string.Join(Environment.NewLine, lines);
+        }
+    }
+}
---
+++ Anime Quote Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿//The purpose of this application is to generate a random anime quote given a rest response from the animechan api
+using Anime_Quote_Generator;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+//Set up the client - Code snippet from https://learn.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear(); //Clear the default headers
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")); //Add the new header
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter"); //Add the user agent header
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    string url = "https://animechan.xyz/api/random";
+    var json = await client.GetStringAsync(url);
+
+    AnimeQuote? quote = JsonConvert.DeserializeObject<AnimeQuote>(json);
+    Console.WriteLine(quote!.ToString());
+}
